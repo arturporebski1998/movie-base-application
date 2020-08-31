ff2044079c2f90c0016f7ec281e3a03a717c5c24
Artur Porębski <A.Porebski@wasko.pl>
2020-08-31 15:49:32 +0200

    small change

--- MovieBaseAngular/src/environments/environment.ts
+++ MovieBaseAngular/src/environments/environment.ts
@@ -4,6 +4,8 @@ export const environment = {
   production: false,
   httpUsers: 'http://localhost:8080/users',
   httpMovies: 'http://localhost:8080/movies',
+  httpManagementUsers: 'http://localhost:8080/management/users',
+  httpManagementMovies: 'http://localhost:8080/management/movies',
   httpOptions: {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
